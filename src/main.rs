0b9372a18b5c90333809f854e37f1d05132713ee
Noah Celuch <noah.celuch@gmail.com>
2026-08-11 22:07:00 -0400

    feat: add basic data structures

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,85 @@
-enum Implementation {
+use std::any::Any;
+use std::fs::File;
+
+enum DatabaseImplementation {
     BTree,
     LSMTree,
 }
 
 struct DatabaseOptions {
     name: String,
-    implementation: Implementation,
+    implementation: DatabaseImplementation,
+}
+
+trait ConfigDBOps {
+    fn create_database(&self, name: String) -> Result<bool, &str>;
+    fn delete_database(&self, name: String) -> Result<bool, &str>;
+}
+
+struct ConfigDB {}
+
+impl ConfigDBOps for ConfigDB {
+    fn create_database(&self, name: String) -> Result<bool, &str> {
+        return Ok(true);
+    }
+
+    fn delete_database(&self, name: String) -> Result<bool, &str> {
+        return Ok(true);
+    }
 }
 
 struct DatabaseProperties {
-    file_path: String,
+    root_path: String,
+    wal: WAL,
 }
 
-struct Database<T: DatabaseTable<R>, R> {
+pub trait DatabaseOps {
+    fn add_table(&self, name: String, fields: Vec<(String, String)>) -> Result<bool, &str>;
+    fn delete_table(&self, name: String) -> Result<bool, &str>;
+}
+
+struct Database<T: DatabaseTable> {
     tables: Vec<T>,
+}
+
+impl<T: DatabaseTable> DatabaseOps for Database<T> {
+    fn add_table(&self, name: String, fields: Vec<(String, String)>) -> Result<bool, &str> {
+        return Ok(true);
+    }
+
+    fn delete_table(&self, name: String) -> Result<bool, &str> {
+        return Ok(true);
+    }
 }
 
 type PrimaryKey = u64;
 
-pub trait DatabaseTable<R> {
-    fn insert(&self, row: R);
-    fn update(&self, key: PrimaryKey, row: R);
-    fn get(&self, key: PrimaryKey) -> Option<R>;
-    fn delete(&self, key: PrimaryKey) -> Option<R>;
+pub trait DatabaseTable {
+    fn insert(&self, row: &dyn Any);
+    fn update(&self, key: PrimaryKey, row: &dyn Any);
+    fn get(&self, key: PrimaryKey) -> Option<Box<dyn Any>>;
+    fn delete(&self, key: PrimaryKey) -> Option<Box<dyn Any>>;
 }
 
 struct LSMTreeDatabaseTable {}
 
-impl<R> DatabaseTable<R> for LSMTreeDatabaseTable {
-    fn insert(&self, row: R) {}
+impl DatabaseTable for LSMTreeDatabaseTable {
+    fn insert(&self, row: &dyn Any) {}
 
-    fn update(&self, key: PrimaryKey, row: R) {}
+    fn update(&self, key: PrimaryKey, row: &dyn Any) {}
 
-    fn get(&self, key: PrimaryKey) -> Option<R> {
+    fn get(&self, key: PrimaryKey) -> Option<Box<dyn Any>> {
         return None;
     }
 
-    fn delete(&self, key: PrimaryKey) -> Option<R> {
+    fn delete(&self, key: PrimaryKey) -> Option<Box<dyn Any>> {
         return None;
     }
+}
+
+struct WAL {
+    file_path: String,
+    file: File,
 }
 
 fn main() {
